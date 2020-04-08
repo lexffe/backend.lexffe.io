3b60e25b315d0b88f85f5129325fb90ed85b4ec8
Alex V <2038611+lexffe@users.noreply.github.com>
2020-04-08 21:07:18 +0100

    refactor: posts, custom_pages, cv -> generic_page

---
+++ handlers/handlers.go
@@ -0,0 +1,3 @@
+package handlers
+
+const paginationLimit = 10
--- models/Page.go
+++ models/Page.go
@@ -1,19 +1,48 @@
 package models
 
 import (
+	"time"
+
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
-// CustomPage is a generic markdown page.
-// The view should render the custom pages in a nav.
-// CV will be put in the same collection as CustomPage. The flag IsCV distinguishes that.
-type CustomPage struct {
-	ID primitive.ObjectID `json:"_id" bson:"_id,omitempty"`
+// Page is a generic markdown page.
+type Page struct {
+	ObjectID primitive.ObjectID `json:"_id" bson:"_id,omitempty"`
+
+	// Title is the title of the page.
 	Title string `json:"title" bson:"title" binding:"required"`
-	FullTitle string `json:"full_title" bson:"full_title"`
-	SearchableTitle string `json:"searchable_title" bson:"searchable_title"`
-	IsCV bool `json:"is_cv" bson:"is_cv" binding:"required"`
+
+	/*
+		SearchableTitle is a generated field that turns the arbitary title into a kebab-case string
+
+		Note: This field is used for MongoDB indexing.
+
+		When a route like /posts/{searchable_title} is used, the controller should use this as filter instead.
+	*/
+	SearchableTitle string `json:"searchable_title" bson:"searchable_title"` // Generated Field
+
+	// Tags is an array of keywords.
+	Tags []string `json:"tags" bson:"tags"`
+
+	// Subtitle is the subtitle/alternative title of the page.
+	Subtitle string `json:"subtitle" bson:"subtitle"`
+
+	// PageType denotes the type of this page
+	PageType ObjectType `json:"page_type" bson:"page_type"`
+
+	// Markdown is the markdown template of the post.
 	Markdown string `json:"markdown" bson:"markdown" binding:"required"`
-	HTML string `json:"html" bson:"html"`
+
+	/*
+		HTML is a generated field from the markdown template.
+		It is used for rendering the blog post itself. (.innerHTML)
+	*/
+	HTML string `json:"html" bson:"html"` // Generated Field
+
+	// Published is a flag for publisher to withhold the post (drafting).
 	Published bool `json:"published" bson:"published" binding:"required"`
+
+	// LastUpdated is a timestamp indicating when the document was last edited.
+	LastUpdated time.Time `json:"last_updated" bson:"last_updated"`
 }
